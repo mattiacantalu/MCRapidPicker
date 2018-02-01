b2742bc43867d434a3b4179c6a17a534a2fed58f
Mattia Cantalù <mattia.cantalu.external@ynap.com>
2018-02-02 00:03:55 +0100

    Initial Commit

---
+++ RapidPickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RapidPickerView
+//
+//  Created by Mattia Cantalù on 02/02/2018.
+//  Copyright © 2018 Mattia Cantalù. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
